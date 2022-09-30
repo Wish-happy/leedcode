1e8c9a4152f5a64a94b1b0a155513a4cef279d5c
liujunhong <2315363569@qq.com>
2022-09-30 10:47:29 +0800

    update

--- hash/0001.two-sum/index.ts
+++ hash/0001.two-sum/index.ts
@@ -1,2 +1,12 @@
 // https://leetcode-cn.com/problems/two-sum
 
+function twoSum(nums: number[], target: number): number[] {
+    let m: { [index: number]: number } = {}
+    for (let i = 0; i < nums.length; i++) {
+        if (m[target - nums[i]] !== undefined) {
+            return [m[target - nums[i]], i]
+        }
+        m[nums[i]] = i
+    }
+    return []
+}
